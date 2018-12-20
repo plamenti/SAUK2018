725bfa7b440561d5edc7bbab73b1f48d5f85ff2e
vkvarbanova23 <45329693+vkvarbanova23@users.noreply.github.com>
2018-12-20 12:33:51 +0200

    Violeta Varbanova - 054-saz

---
+++ VioletaVarbanova - 054saz.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestClass]
+    public class LogInWithUnexistingAccount
+    {
+        private static IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private static string baseURL;
+        private bool acceptNextAlert = true;
+        
+        [ClassInitialize]
+        public static void InitializeClass(TestContext testContext)
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.katalon.com/";
+        }
+        
+        [ClassCleanup]
+        public static void CleanupClass()
+        {
+            try
+            {
+                //driver.Quit();// quit does not close the window
+                driver.Close();
+                driver.Dispose();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+        }
+        
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            verificationErrors = new StringBuilder();
+        }
+        
+        [TestCleanup]
+        public void CleanupTest()
+        {
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+        
+        [TestMethod]
+        public void TheLogInWithUnexistingAccountTest()
+        {
+            driver.Navigate().GoToUrl("https://www.abv.bg/");
+            driver.FindElement(By.Id("username")).Click();
+            driver.FindElement(By.Id("username")).Clear();
+            driver.FindElement(By.Id("username")).SendKeys("test");
+            driver.FindElement(By.Id("password")).Clear();
+            driver.FindElement(By.Id("password")).SendKeys("testtest");
+            driver.FindElement(By.Id("loginBut")).Click();
+            try
+            {
+                Assert.AreEqual("Грешен потребител / парола.", driver.FindElement(By.Id("form.errors")).Text);
+            }
+            catch (Exception e)
+            {
+                verificationErrors.Append(e.Message);
+            }
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+        
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+        
+        private string CloseAlertAndGetItsText() {
+            try {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert) {
+                    alert.Accept();
+                } else {
+                    alert.Dismiss();
+                }
+                return alertText;
+            } finally {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
